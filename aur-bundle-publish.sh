174964a996505d5713c0b730bdd703cb4b6ba8ec
Bernardo Kuri <github@bkuri.com>
2023-12-10 18:13:24 -0600

    update bundle script

--- aur-bundle-publish.sh
+++ aur-bundle-publish.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-ACCESS_TOKEN="your_personal_access_token"
-REPO_OWNER="bkuri"
-
 if [ ! -f PKGBUILD ]; then
   echo "Error: PKGBUILD file not found."
   exit 1
@@ -15,7 +12,7 @@ makepkg -si || { echo "Error: Failed to build and install the package."; exit 1;
 
 # Create a release
 curl -X POST \
-  -H "Authorization: token ${ACCESS_TOKEN}" \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/${REPO_OWNER}/${pkgname}/releases \
+  https://api.github.com/repos/${GITHUB_REPOSITORY_OWNER}/${pkgname}/releases \
   -d '{"tag_name": "'"${pkgver}"'","name": "'"${pkgver}"'"}'
